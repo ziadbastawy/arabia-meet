fd44a569db738dc0714347f8d776370f9596468a
ziadmahmoud <ziadmahmoud413@gmai.com>
2023-01-29 12:08:43 +0200

    start work on auth layout

--- Layouts/auth.vue
+++ Layouts/auth.vue
@@ -1,6 +1,28 @@
+<script setup>
+import Login from "@/assets/images/login.vue";
+</script>
+
 <template>
-    <div>
-        AUTH LAYOUT 
+  <div class="container">
+    <div class="box bg-white d-flex">
+      <div class="w-60 d-flex justify-content-between align-items-center">
+        <Login />
+      </div>
+      <div class="w-40 bg-neutral-2 h-full">
+        Form
         <slot />
+      </div>
     </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.container {
+  margin: 10vh auto;
+  .box {
+    max-width: 1280px;
+    // background-color: rgb(186, 220, 255);
+    min-height: 80vh;
+  }
+}
+</style>
